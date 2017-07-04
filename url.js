d9111cbddd5befc85c47f9ed9de01888f4d8c0e7
郭晋良 <guojinliang@ifanr.com>
2017-07-04 16:14:33 +0800

    add validate/url module and rename files

---
+++ url.js
@@ -0,0 +1,18 @@
+/**
+ * 格式化 URL 参数
+ * @param  {String} url    URL
+ * @param  {Object} params 参数
+ * @return {String}        转换参数后的 URL
+ */
+const formatURL = (url, params) => {
+  params = params || {};
+  for (var key in params) {
+    var reg = new RegExp(':' + key, 'g');
+    url = url.replace(reg, params[key]);
+  }
+  return url.replace(/[^:]\/\//g, '/');
+};
+
+module.exports = {
+  formatURL,
+};
